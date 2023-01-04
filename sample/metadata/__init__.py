fb684c7c3d72b7cf3ee13f3cf13ec8caea22aea6
user.name <gmail@hdknr.com>
2023-01-04 16:22:57 +0900

    first migrations

--- sample/metadata/__init__.py
+++ sample/metadata/__init__.py
@@ -1,5 +1,9 @@
 from models.tickit import metadata as tickit_metadata
-from models.tpcds import metadata as tpcds_metadata
-from models.tpch import metadata as tpch_metadata
 
-metadata = [tpch_metadata, tpcds_metadata, tickit_metadata]
+# from models.tpcds import metadata as tpcds_metadata
+# from models.tpch import metadata as tpch_metadata
+
+# metadata = [tpch_metadata, tpcds_metadata, tickit_metadata]
+metadata = [
+    tickit_metadata,
+]
